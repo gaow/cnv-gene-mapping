afa05068989096657b4bb930b0cc2ae827c95bd5
Min Qiao <qiaomin312@gmail.com>
2017-07-22 00:50:16 -0500

    add meeting notes 07212017

--- docs/jnbinder.py
+++ docs/jnbinder.py
@@ -600,7 +600,6 @@ def make_index_nb(path, exclude):
         name = os.path.splitext(os.path.basename(fn))[0].replace('_', ' ')
         with open(fn) as f:
             data = json.load(f)
-        title = re.compile('(^\W+|\W+$)').sub('', data["cells"][0]["source"][0]).strip()
         out += '''
   {
    "cell_type": "markdown",
@@ -609,7 +608,8 @@ def make_index_nb(path, exclude):
     "[%s](%s/%s)<br>\\n",
     "&nbsp; &nbsp; %s"
    ]
-  },''' % (name, path, os.path.splitext(os.path.basename(fn))[0] + '.html', title.replace('"', "'"))
+  },''' % (name, path, os.path.splitext(os.path.basename(fn))[0] + '.html',
+           data["cells"][0]["source"][0].replace('"', "'").strip("#"))
     if len(sos_files):
         out += '''
   {
@@ -688,3 +688,23 @@ def make_empty_nb(name):
  "nbformat": 4,
  "nbformat_minor": 2
 }''' % name
+
+def compare_versions(v1, v2):
+    # This will split both the versions by '.'
+    arr1 = v1.split(".")
+    arr2 = v2.split(".")
+    # Initializer for the version arrays
+    i = 0
+    # We have taken into consideration that both the
+    # versions will contains equal number of delimiters
+    while(i < len(arr1)):
+        # Version 2 is greater than version 1
+        if int(arr2[i]) > int(arr1[i]):
+            return -1
+        # Version 1 is greater than version 2
+        if int(arr1[i]) > int(arr2[i]):
+            return 1
+        # We can't conclude till now
+        i += 1
+    # Both the versions are equal
+    return 0
