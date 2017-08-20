35f31793cceaa0b2a9f4000a04c29402bed006b3
Min Qiao <eqiaomin@gmail.com>
2017-08-20 13:42:54 -0500

    Reorganize notebooks; Implement DAP analysis on blocks

--- utils/extract_function.py
+++ utils/extract_function.py
@@ -1,15 +1,22 @@
-import argparse, re
 '''
-Usage: python extract_function.py --from A.py --to B.py
+Usage: python extract_function.py --from A.ipynb --to A.py B.py C.py
 '''
 if __name__ == '__main__':
+    import argparse, re
+    import json
     parser = argparse.ArgumentParser()
     parser.add_argument("--from", dest = "source")
-    parser.add_argument("--to")
+    parser.add_argument("--to", nargs = '+')
     args = parser.parse_args()
     output = []
+    source = []
     harvest = False
-    for line in open(args.source).readlines():
+    for cell in json.load(open(args.source))['cells']:
+        if cell['cell_type'] == 'code':
+            source.extend([x.rstrip() for x in cell['source']])
+    for line in source:
+        if line.startswith('#'):
+            continue
         if line.startswith('import') or line.startswith('from'):
             output.append(line)
         if line.startswith('def') or line.startswith('class'):
@@ -22,5 +29,6 @@ if __name__ == '__main__':
             else:
                 output.append('\n')
                 harvest = False
-    with open(args.to, 'w') as f:
-        f.write(''.join(output))
+    for item in args.to:
+        with open(item, 'w') as f:
+            f.write('\n'.join(output))
